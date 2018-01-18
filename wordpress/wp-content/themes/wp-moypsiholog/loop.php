98fc5fc40857e97f03389f5395e02ac24b9fa1e8
Vitalii A <crazyyy@gmail.com>
2018-01-18 15:21:42 +0200

    fixes

--- wordpress/wp-content/themes/wp-moypsiholog/loop.php
+++ wordpress/wp-content/themes/wp-moypsiholog/loop.php
@@ -1,5 +1,5 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-  <div id="post-<?php the_ID(); ?>" <?php post_class('item mt-32 mb-32 clearfix'); ?>>
+  <div id="post-<?php the_ID(); ?>" <?php post_class('item-paddinger item mt-32 mb-32 clearfix'); ?>>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <div class="text">
       <div class="submitted"><?php the_date('d.m.Y'); ?></div>
